4531f3474ef9bd6fce76425009320affb2620670
matheuzcapdevila <matheuz.lcc@gmail.com>
2022-01-16 13:34:59 -0300

    Adicionando exemplo do uso do continue e break

--- DigitalInnovationOne/Program.cs
+++ DigitalInnovationOne/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Iteration {i}: ");
+    
+    if (i < 3)
+    {
+        Console.WriteLine("skip");
+        continue;
+    }
+    
+    Console.WriteLine("done");
+}
